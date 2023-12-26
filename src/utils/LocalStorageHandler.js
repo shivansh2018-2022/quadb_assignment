24da1cadc21d5c5f45b08549e3ca92c522eae947
shivansh verma <shivansh.verma1999@gmail.com>
2023-12-26 11:28:11 +0530

    add state persistence using local storage and prevent routing to a blank page

---
+++ src/utils/LocalStorageHandler.js
@@ -0,0 +1,11 @@
+// Make this class a singleton
+export class LocalStorageHandler {
+    saveItem = (key, value) => {
+      localStorage.setItem(key, value)
+    }
+  
+    getItem = (key) => {
+      return localStorage.getItem(key)
+    }
+  }
+  
